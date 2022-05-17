740669e7877ed281047109939a44eef865b74465
George.P <georgipetroov@gmail.com>
2022-05-17 18:00:34 +0300

    implemented the db  connection

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+import util.JdbcUtil;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Application {
+    public static void main(String[] args) throws IOException, SQLException {
+        Properties properties = new Properties();
+        String dbConfigPath = Application.class.getClassLoader()
+                .getResource("jdbc.properties").getPath();
+        properties.load(new FileInputStream(dbConfigPath));
+        Connection connection = JdbcUtil.createDbConnection(properties);
+    }
+}
