9c9981ca432224c7536f648d5294511147787cf4
George.P <georgipetroov@gmail.com>
2022-05-18 18:35:18 +0300

    changed the invalid password message to not formatted

--- src/main/java/dialog/UserLoginDialog.java
+++ src/main/java/dialog/UserLoginDialog.java
@@ -10,7 +10,7 @@ public class UserLoginDialog {
     public static final int MAX_USER_LENGTH = 15;
     public static final String INVALID_LENGTH = "Username length must be between 3 and 15 symbols long.";
     public static final String INVALID_PASSWORD =
-            "Password '%s' is invalid. " +
+            "Password is invalid. " +
             "Password must be between 8 and 15 symbols long, " +
             "at least one digit, one capital letter, " +
             "and one sign different than letter or digit.";
