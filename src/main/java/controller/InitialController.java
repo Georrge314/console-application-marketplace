03ddad4babf2bae285ad0388a4d6fe64526a4286
George.P <georgipetroov@gmail.com>
2022-05-21 17:39:04 +0300

    added a user service and caught the exceptions that may cause its methods

--- src/main/java/controller/InitialController.java
+++ src/main/java/controller/InitialController.java
@@ -4,24 +4,49 @@ import dialog.UserLoginDialog;
 import dialog.UserRegisterDialog;
 import dialog.dto.UserLoginDto;
 import dialog.dto.UserRegisterDto;
+import exception.EntityNotFoundException;
+import exception.InvalidEntityException;
 import menu.Menu;
 import menu.Option;
+import model.service.UserServiceModel;
+import service.UserService;
+import util.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static util.ObjectMapper.*;
+
 public class InitialController {
+    private final UserService userService;
+
+    public InitialController(UserService userService) {
+        this.userService = userService;
+    }
 
     public void init() {
         Menu menu = new Menu("Welcome to Marketplace", new ArrayList<>(List.of(
                 new Option("Register", () -> {
                     UserRegisterDto userRegisterDto = new UserRegisterDialog().input();
-                    System.out.println(userRegisterDto);
+                    UserServiceModel model = MODEL_MAPPER.map(userRegisterDto, UserServiceModel.class);
+                    try {
+                        userService.createUser(model);
+                        System.out.println("Register successfully");
+                    } catch (InvalidEntityException e) {
+                        System.out.println("Register failed");
+                        System.out.println(e.getMessage());
+                    }
                     return "";
                 }),
                 new Option("Login", () -> {
                     UserLoginDto userLoginDto = new UserLoginDialog().input();
-                    System.out.println(userLoginDto);
+                    try {
+                        userService.getUserByUsernameAndPassword(userLoginDto.getUsername(), userLoginDto.getPassword());
+                        System.out.println("Login successfully");
+                    } catch (EntityNotFoundException e) {
+                        System.out.println("Login failed");
+                        System.out.println(e.getMessage());
+                    }
                     return "";
                 }),
                 new Option("Skip Login/Register", () -> {
