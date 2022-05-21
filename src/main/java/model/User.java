e17d9954427aa20a836582bc031badc232c3ee56
George.P <georgipetroov@gmail.com>
2022-05-21 17:19:36 +0300

    added user model

---
+++ src/main/java/model/User.java
@@ -0,0 +1,106 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    private String pictureUrl;
+    private boolean isAdmin;
+    private boolean isActive;
+    private double balance;
+    private final Timestamp created = new Timestamp(System.currentTimeMillis());
+    private final Timestamp modified = new Timestamp(System.currentTimeMillis());
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public Timestamp getModified() {
+        return modified;
+    }
+}
