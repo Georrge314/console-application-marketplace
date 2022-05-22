f399117e1a67f92f0556fb09364a6d73c769a4d4
George.P <georgipetroov@gmail.com>
2022-05-22 17:10:35 +0300

    implented new methods from interface

--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import model.User;
 import model.service.UserServiceModel;
 import service.UserService;
 
+import java.util.List;
+
 import static util.ObjectMapper.*;
 
 public class UserServiceImpl implements UserService {
@@ -25,5 +27,15 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserByUsernameAndPassword(String username, String password) throws EntityNotFoundException {
         return userRepo.findUserByUsernameAndPassword(username, password);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public User getByUsername(String username) throws EntityNotFoundException {
+        return userRepo.findByUsername(username);
     }
 }
