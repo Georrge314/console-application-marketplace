fc641394868d89d5d577a955dfe75feb92533915
George.P <georgipetroov@gmail.com>
2022-05-22 17:16:24 +0300

    added main controller

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import menu.Menu;
+import menu.Option;
+import service.ServiceContainer;
+import system.SystemUser;
+
+import java.util.ArrayList;
+import java.util.List;
+public class MainController {
+    public void init() {
+        List<Option> options = new ArrayList<>(List.of(
+                new Option("Stores", () -> {
+                    return "";
+                }),
+                new Option("Categories", () -> {
+                    return "";
+                }),
+                new Option("Products", () -> {
+                    return "";
+                }),
+                new Option("Users", () -> {
+                    return "";
+                }),
+                new Option("About", () -> {
+                    return "System information";
+                })
+        ));
+        if (SystemUser.isLoggedIn()) {
+            //TODO add other options
+            options.add(
+                    new Option("Account", () -> {
+                new AccountController(ServiceContainer.getUserService()).init();
+                return "";
+            }));
+
+            if (SystemUser.isAdmin()) {
+                options.add(new Option("Admin Menu", () -> {
+                    new AdminController(ServiceContainer.getUserService()).init();
+                    return "";
+                }));
+            }
+        }
+
+        Menu menu = new Menu("Main Menu", options);
+        menu.show();
+    }
+}
