333635dcd81107f841eed31a1f89118f9facebae
George.P <georgipetroov@gmail.com>
2022-05-22 17:15:21 +0300

    added user repo interface

--- src/main/java/dao/UserRepository.java
+++ src/main/java/dao/UserRepository.java
@@ -4,7 +4,12 @@ import exception.EntityNotFoundException;
 import exception.InvalidEntityException;
 import model.User;
 
+import java.util.List;
+
 public interface UserRepository {
     User createUser(User user) throws InvalidEntityException;
     User findUserByUsernameAndPassword(String username, String password) throws EntityNotFoundException;
+
+    List<User> findAll();
+    User findByUsername(String username) throws EntityNotFoundException;
 }
