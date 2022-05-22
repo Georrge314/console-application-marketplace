f81089712901f205974cd1fd3b46126aae9d4e8c
George.P <georgipetroov@gmail.com>
2022-05-22 17:25:32 +0300

    added dialog wich takes the username input from the console and returns it to the manage controller

---
+++ src/main/java/dialog/TypeUsernameDialog.java
@@ -0,0 +1,21 @@
+package dialog;
+
+import java.util.Scanner;
+
+import static dialog.UserLoginDialog.*;
+
+public class TypeUsernameDialog {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String input() {
+        String username = "";
+        while (username.isBlank()) {
+            System.out.println("Enter username");
+            String answer = scanner.nextLine();
+            if (answer.length() >= MIN_USER_LENGTH && answer.length() <= MAX_USER_LENGTH) {
+                username = answer;
+            }
+        }
+        return username;
+    }
+}
